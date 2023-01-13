185f1250a4aa4e40f6bed9c5f2af6d72b95e9099
OksanaVorobiova <ov172608@gmail.com>
2023-01-13 12:16:06 +0200

    task-2 completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onTimeupdate, 1000));
+
+const SECONDS = localStorage.getItem("videoplayer-current-time:");
+console.log(SECONDS);
+player.setCurrentTime(SECONDS).then(function(SECONDS) {
+    // seconds = the actual time that the player seeked to
+    console.log(SECONDS);
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            console.log(error.message);
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            console.log(error.name);
+            // some other error occurred
+            break;
+    }
+});
+
+
+function onTimeupdate(e) {
+        localStorage.setItem("videoplayer-current-time:", e.seconds);
+}
