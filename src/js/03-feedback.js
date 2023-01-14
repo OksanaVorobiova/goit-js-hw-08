eff279df70079a74a72b455562dbf493fc670f6b
OksanaVorobiova <ov172608@gmail.com>
2023-01-14 19:36:52 +0200

    Homework completed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
+import throttle from 'lodash.throttle'
 
+
+const refs = {
+    formEl: document.querySelector('.feedback-form'),
+    inputEl: document.querySelector('input[name="email"]'),
+    textarea: document.querySelector('textarea'),
+}
+
+const { formEl, inputEl, textarea, } = refs;
+const STORAGE_KEY = "feedback-form-state";
+const data = {};
+
+formEl.addEventListener('input', throttle(onFormInput, 500));
+window.addEventListener('DOMContentLoaded', onRefresh);
+formEl.addEventListener('submit', onFormSubmit);
+
+
+function onFormInput(e) {
+    data[e.target.name] = e.target.value;
+    
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+    
+}
+
+
+function onRefresh() {
+   const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+    if(savedData !== {}) {
+        inputEl.value = savedData.email;
+        textarea.value = savedData.message;
+    } 
+}
+
+function onFormSubmit(ev) {
+    ev.preventDefault();
+    window.removeEventListener('DOMContentLoaded', onRefresh);
+
+    const {
+        elements: { email, message }
+    } = ev.currentTarget;
+    
+    console.log({email: email.value, message: message.value});
+    localStorage.removeItem(STORAGE_KEY);
+    ev.currentTarget.reset();
+
+}
+
+
+ 
